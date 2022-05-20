4e3d3be5cd01d5325fbc0be75cce7ab1d20aee4a
Jacegk1999 <Jacegk1999@gmail.com>
2022-05-19 17:53:01 -0700

    auth page

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from 'react';
-import { useEffect } from 'react';
 import { getUser } from '../services/user';
 
 const UserContext = createContext();
@@ -7,25 +6,12 @@ const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(getUser());
  
 
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    const getUserData = async () => {
-      const user = await fetchProfileData();
-      user && setProfile(user[0]);
-    };
-    getUserData();
-  }, []);
 
   return (
     <UserContext.Provider
       value={{
         currentUser,
         setCurrentUser,
-        userName,
-        setUserName,
-        setProfile,
-        profile,
       }}
     >
       {children}
@@ -35,7 +21,7 @@ const UserProvider = ({ children }) => {
 const useUserContext = () => {
   const context = useContext(UserContext);
   if (context === undefined) {
-    throw new Error('useRestaurant context must be used within a provider');
+    throw new Error('useUser context must be used within a provider');
   }
   return context;
 };
